b77fee59fefe3384aca13a4542c330ce1c4d5b48
Neko Of The Abyss <psi@innocent.com>
2023-01-31 11:49:11 +0530

    add bot

--- serve.ts
+++ serve.ts
@@ -5,7 +5,11 @@ import { Database } from "sqlite3";
 
 import { logger } from "./log.ts";
 
-import { create } from "https://deno.land/x/id_gen@v1.1.0/mod.ts";
+import {
+  Client,
+  Embed,
+  GatewayIntents,
+} from "https://deno.land/x/harmony@v2.8.0/mod.ts";
 
 const SCARDS_EPOCH = 1667759400000;
 
@@ -56,6 +60,33 @@ const addPass = db.prepare(
     agree_to_terms,
     reference_id
     ) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+);
+
+const getTeamDetails = db.prepare(
+  `SELECT 
+    team_name, 
+    team_members, 
+    all_passes,
+    event_name, 
+    contact_number, 
+    email_id, 
+    institution_name, 
+    degree_and_branch,
+    agree_to_terms,
+    reference_id
+    FROM teams WHERE reference_id = ?`,
+);
+
+const getPassDetails = db.prepare(
+  `SELECT 
+    name, 
+    contact_number, 
+    email_id, 
+    institution_name, 
+    degree_and_branch,
+    agree_to_terms,
+    reference_id
+     FROM all_pass WHERE reference_id = ?`,
 );
 
 const verifyPass = db.prepare(`SELECT name FROM all_pass WHERE reference_id=?`);
@@ -204,3 +235,50 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.listen({ port: 6123 });
+
+const DISCORD_TOKEN = Deno.env.get("LALA_TOKEN");
+
+const client = new Client({
+  token: DISCORD_TOKEN,
+  disableEnvToken: true,
+  intents: [
+    GatewayIntents.GUILDS,
+    GatewayIntents.MESSAGE_CONTENT,
+    GatewayIntents.GUILD_MESSAGES,
+  ],
+});
+
+const GET_COMMAND = /^get\s(P|R)-(\d+)/i;
+
+client.on("messageCreate", (message) => {
+  if (message.channelID !== "1069859960428707954") return;
+  if (GET_COMMAND.exec(message.content)) {
+    const args = GET_COMMAND.exec(message.content);
+    if (args) {
+      const data = args[1].toLowerCase() === "p"
+        ? getPassDetails.all(args[2])
+        : getTeamDetails.all(args[2]);
+
+      for (const row of data) {
+        const e = new Embed().setColor("#c39232");
+        let res = `\nREF: ${args[2]}`;
+        if (args[1].toLowerCase() === "p") {
+          res += `\nNAME: ${row.name}`;
+        } else {
+          res += `\nTEAM: ${row.team_name}`;
+          res += `\nPASSES: ${row.all_passes}`;
+          res += `\nEVENT: ${row.event_name}`;
+        }
+        res += `\nCONTACT: ${row.contact_number}`;
+        res += `\nEMAIL: ${row.email_id}`;
+        res += `\nINSTITUTION: ${row.institution_name}`;
+        res += `\nDEGREE: ${row.degree_and_branch}`;
+        res += `\nAGREED TO TERMS: ${row.agree_to_terms}`;
+        e.setDescription(res);
+        message.channel.send({ embeds: [e] });
+      }
+    }
+  }
+});
+
+client.connect();
